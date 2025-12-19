b8b3eec361e87f801d3a3ada2980b44058b089dc
Arul <aruldhanasekards@gmail.com>
2025-12-19 10:59:06 +0530

    Initial project upload

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -38,7 +38,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
   // Demo mode configuration
   const DEMO_USER = {
-    uid: 'AVS5YObxtBXgyIcGmSCewqiT0623',
+    uid: 'Hib1lTkAUpbIRN4wEU3ddyOURoj1',
     email: 'arul@useoutpostmail.com',
     emailVerified: true,
   } as User;
