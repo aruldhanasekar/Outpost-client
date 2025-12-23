d792f1f6fd1f59cbaa6cf1f81ccf21a45a506ae6
Arul <aruldhanasekards@gmail.com>
2025-12-23 10:07:18 +0530

    added scheduled page

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,13 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    proxy: {
+      '/api': {
+        target: 'https://outpost-server-outpost.up.railway.app',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
