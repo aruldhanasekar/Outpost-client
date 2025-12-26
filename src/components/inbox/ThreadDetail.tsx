dba288e401fb44b99af48f447f773828c7729687
Arul <aruldhanasekards@gmail.com>
2025-12-26 17:46:46 +0530

    added custom triage email

--- src/components/inbox/ThreadDetail.tsx
+++ src/components/inbox/ThreadDetail.tsx
@@ -272,10 +272,10 @@ function CategoryDropdownMenu({ thread, emails, userEmail, onCategoryChange }: C
       {/* Logo trigger button */}
       <button
         onClick={() => setIsOpen(!isOpen)}
-        className="p-1 rounded-lg transition-colors"
+        className="p-1.5 hover:bg-gray-100 rounded-lg transition-colors"
         title="Change category"
       >
-        <img src={OutpostLogo} alt="Outpost" className="w-9 h-9" />
+        <img src={OutpostLogo} alt="Outpost" className="w-7 h-7" />
       </button>
       
       {/* Dropdown menu */}
